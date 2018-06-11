aaddea7dd19f538195fb4ad0cdb755ac8f28d40c
John Porto <john.porto@rocketmail.com>
2018-06-11 08:15:23 -0700

    working through the Dicey Business lab

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-class Dice {
-    constructor(){
-        this.div = document.createElement[('die');
-        div.className = 'dice']
+const MAX = 800;
+
+class Square {
+    constructor(x, y){
+        this.div = document.createElement('div');
+        this.div.classList.add('square');
+        this.div.style.left = '$(x)px';
+        this.div.style.top = '$(y)px';
 
     }
 }
+
+
+
+let diceButton = document.getElementById('diceButton');
+diceButton.addEventListener('click', insertSquare);
+
+function insertSquare() {
+    let xVal = (100);
+    let yVal = (200);
+}
+
+
+// function randomVal(min, max) {
+//     math.floor(Math.random() * 6) + 1
+// }
+
+
+
+
+
+// class Die {
+//     constructor(){
+//         this.div = document.createElement[('div');
+//         div.className = 'dice']
+
+//     }
+// }
