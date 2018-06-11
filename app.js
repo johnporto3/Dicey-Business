4ad6ff127b9cb6471b91d8407c3dd036108a3689
John Porto <john.porto@rocketmail.com>
2018-06-11 13:12:20 -0700

    still working on this

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
-const MAX = 800;
-
-class Square {
+let container = document.getElementById('dice-container')
+class Die {
     constructor(x, y){
         this.div = document.createElement('div');
         this.div.classList.add('square');
         this.div.style.left = '$(x)px';
         this.div.style.top = '$(y)px';
+        container.appendChild(this.div);
 
     }
 }
-
-
-
 let diceButton = document.getElementById('diceButton');
 diceButton.addEventListener('click', insertSquare);
 
 function insertSquare() {
     let xVal = (100);
     let yVal = (200);
+    let sq = new Die(xVal, yVal);
 }
 
-
-// function randomVal(min, max) {
-//     math.floor(Math.random() * 6) + 1
-// }
-
-
-
-
-
-// class Die {
-//     constructor(){
-//         this.div = document.createElement[('div');
-//         div.className = 'dice']
-
-//     }
-// }
+function randomVal(min, max) {
+    math.floor(Math.random() * 6) + 1
+}
